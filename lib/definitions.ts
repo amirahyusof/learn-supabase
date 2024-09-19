595156ad4355e094424670c02fd6b4277e97ad18
amirah <mirez.data@gmail.com>
2024-09-19 17:06:28 +0800

    add definition

---
+++ lib/definitions.ts
@@ -0,0 +1,11 @@
+export type ListBook = {
+  id: string;
+  title: string;
+  author: string;
+  genre: string;
+  readingStatus: 'reading' | 'want to read' | 'finished'
+}
+
+export type Categories = {
+  
+}
