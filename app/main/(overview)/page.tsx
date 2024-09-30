c17973658692e6b53782054935161e096e5791e8
amirah <mirez.data@gmail.com>
2024-09-30 10:04:53 +0800

    commit changes in main

---
+++ app/main/(overview)/page.tsx
@@ -0,0 +1,12 @@
+import MainPage from '@/components/main-page'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  )
+}
+
+export default page
