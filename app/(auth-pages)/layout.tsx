91c39a33e161eb74a61c197a9d07f521c858af2c
amirah <mirez.data@gmail.com>
2024-10-02 08:43:21 +0800

    modifity auth

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,10 +1,10 @@
 export default async function Layout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <div className="max-w-7xl flex flex-col gap-12 items-start">
+    <div className="h-screen flex flex-col justify-center items-center">
       {children}
     </div>
   );
--- app/(auth-pages)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth-pages)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,12 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn, useUser } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  const {user} = useUser()
+
+  if(!user){
+    return <SignIn />
+  }
+
+  return <div>Welcome!</div>
+  
 }
