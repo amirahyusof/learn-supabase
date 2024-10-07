7e9d41a67f83e8108f1b08962c15aed39e16ad43
amirah <mirez.data@gmail.com>
2024-10-07 10:56:36 +0800

    changes and fix header and nav bar's layout

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -20,19 +20,19 @@ export default function Header(){
         </SheetTrigger>
         <SheetContent side="top" className="flex flex-col">
           <nav className="grid gap-2 text-lg font-light">
-            <Link href="/dashboard" className="mx-[-0.65rem] flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+            <Link href="/dashboard" className="mt-4 mx-[-0.65rem] flex items-center gap-3 rounded-lg px-3 py-2  bg-pink-300 text-white transition-all hover:bg-pink-600 hover:text-primary">
               <Home className="h-4 w-4" />
                 Main
             </Link>
-            <Link href="/dashboard/all-books" className="mx-[-0.65rem] flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary">
+            <Link href="/dashboard/all-books" className="mx-[-0.65rem] flex items-center gap-3 rounded-lg bg-pink-300 text-white px-3 py-2 text-primary transition-all hover:text-primary hover:bg-pink-600">
               <BookText className="h-4 w-4" />
                 All Book
             </Link>
-            <Link href="/dashboard/categories" className="mx-[-0.65rem] flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary">
+            <Link href="/dashboard/categories" className="mx-[-0.65rem] flex items-center gap-3 rounded-lg  bg-pink-300 text-white px-3 py-2 text-primary transition-all hover:text-primary hover:bg-pink-600">
               <LibraryBig className="h-4 w-4" />
                 Categories
             </Link>
-            <Link href="/dashboard/add-book" className="mx-[-0.65rem] flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary">
+            <Link href="/dashboard/add-book" className="mx-[-0.65rem] flex items-center gap-3 rounded-lg  bg-pink-300 text-white px-3 py-2 text-primary transition-all hover:text-primary hover:bg-pink-600">
               <BookKey className="h-4 w-4" />
                 Add Book
             </Link>
